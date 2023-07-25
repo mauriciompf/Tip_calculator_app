6156628e1bc853bafe0681d6126f39b1fc50a34b
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-07-25 16:09:45 -0400

    Update app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,40 +1,81 @@
 const bill = document.querySelector("#bill");
 const numberOfPeople = document.querySelector("#number-of-people");
 const buttons = document.querySelectorAll("#buttons > button");
+const tipAmountElem = document.querySelector("#tip-amount");
+const totalElem = document.querySelector("#total");
+const resetButton = document.querySelector("#reset-btn");
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    const currentButton = e.currentTarget;
-    const percentageTip = parseInt(currentButton.value) / 100;
+    if (validateInputs()) {
+      const currentButton = e.currentTarget;
+      const percentageTip = parseInt(currentButton.value) / 100;
+      resetButton.disabled = false;
+      resetButton.classList.remove("cursor-not-allowed");
+      resetButton.addEventListener("click", resetCalculator);
 
-    // Custom button --> Custom input
-    if (isNaN(percentageTip)) {
-      const custom = document.createElement("input");
-      custom.setAttribute("type", "number");
-      custom.setAttribute(
-        "class",
-        "focus:outline-2 hover:outline-2 focus:outline-primary-stron-cyan text-neutral-very-dark-cyan bg-neutral-very-light-grayish-cyan pr-2 pl-8 rounded-md text-right text-[1.25rem] w-[107px] caret-color"
-      );
-      button.parentElement.appendChild(custom);
-      custom.select();
-      document.querySelector("#custom").remove();
+      if (isNaN(percentageTip)) {
+        const custom = document.createElement("input");
+        custom.setAttribute("type", "number");
+        custom.setAttribute(
+          "class",
+          "focus:outline-2 hover:outline-2 focus:outline-primary-stron-cyan text-neutral-very-dark-cyan bg-neutral-very-light-grayish-cyan pr-2 pl-8 rounded-md text-right text-[1.25rem] md:w-[107px] w-full caret-color"
+        );
+        button.parentElement.appendChild(custom);
+        custom.select();
+        document.querySelector("#custom").remove();
+        custom.addEventListener("keypress", (e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            calculateTip(custom.value);
+          }
+        });
+      } else {
+        calculateTip(currentButton.value);
+      }
     }
-
-    // console.log(typeof currentButton.value);
-    const totalTip = bill.value * percentageTip;
-    let tipAmount = totalTip / numberOfPeople.value;
-    tipAmount = (Math.floor(tipAmount * 100) / 100).toFixed(2);
-    const total = (
-      (bill.value * (percentageTip + 1)) /
-      numberOfPeople.value
-    ).toFixed(2);
-
-    console.log(tipAmount, total);
   });
 });
 
-// Bill x percentageTip = Ttip
+function calculateTip(value) {
+  const percentageTip = parseInt(value) / 100;
+  // console.log(typeof currentButton.value);
+  const totalTip = bill.value * percentageTip;
+  let tipAmount = totalTip / numberOfPeople.value;
+  tipAmount = (Math.floor(tipAmount * 100) / 100).toFixed(2);
+  const total = (
+    (bill.value * (percentageTip + 1)) /
+    numberOfPeople.value
+  ).toFixed(2);
 
-// Tip Amount \ per person = Ttip / Npeople
+  tipAmountElem.textContent = tipAmount;
+  totalElem.textContent = total;
+}
 
-// Total \ per person = Bill * (percentageTip + 1) / Npeople
+function resetCalculator() {
+  tipAmountElem.textContent = "$0.00";
+  totalElem.textContent = "$0.00";
+  resetButton.disabled = true;
+  resetButton.classList.add("cursor-not-allowed");
+  bill.value = "";
+  numberOfPeople.value = "";
+}
+
+function validateInputs() {
+  const error = document.querySelector("#error");
+  let isValid;
+
+  if (numberOfPeople.value === "0" || numberOfPeople.value === "") {
+    error.textContent = "Can't be zero";
+    numberOfPeople.classList.add("outline", "outline-1", "outline-red-500");
+    isValid = false;
+  } else {
+    error.textContent = "";
+    numberOfPeople.classList.remove("outline", "outline-1", "outline-red-500");
+    isValid = true;
+  }
+
+  if (bill.value === "0" || bill.value === "") alert("Something is wrong...");
+
+  return isValid;
+}
